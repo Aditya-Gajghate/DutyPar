13a294ad2ef167686947b5019d4547a9542d22bd
kiranbarkade <kiranbarkade2022@gmail.com>
2026-04-23 20:43:22 +0530

    ghe re zhatu tuza code

--- app/page.tsx
+++ app/page.tsx
@@ -19,21 +19,21 @@ export default function Home() {
             To get started, edit the page.tsx file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            Ye le tera bread aoure ye le tera bread{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-              Templates
+              nahi
             </a>{" "}
-            or the{" "}
+            chaiye{" "}
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-              Learning
+             zatu
             </a>{" "}
-            center.
+           chal
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
