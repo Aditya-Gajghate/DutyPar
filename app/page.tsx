d9b3d45b82297816d95848fd7ed441f768a60b1f
kiranbarkade <kiranbarkade2022@gmail.com>
2026-04-23 20:48:37 +0530

    update ghe re zhatu

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
             To get started, edit the page.tsx file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Ye le tera bread aoure ye le tera bread{" "}
+            Ye le tera bread aoure ye le tera dudh{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
@@ -31,9 +31,9 @@ export default function Home() {
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-             zatu
+             mereko
             </a>{" "}
-           chal
+           
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
